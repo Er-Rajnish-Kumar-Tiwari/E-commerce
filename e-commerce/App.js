8ddb3380b7f543241f6041e4724667ba1dc75551
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2025-08-13 19:23:57 +0100

    reduxfinal

--- e-commerce/App.js
+++ e-commerce/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import store from "./store";
 import StackNavigator from "./navigation/stackNavigator";
 import FlashMessage from "react-native-flash-message";
 import { SafeAreaProvider } from "react-native-safe-area-context";
